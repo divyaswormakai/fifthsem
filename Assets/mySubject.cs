b398c4c66d56d78cb8cafd7cb62b1e4efc8599e1
Divyaswor Makai <makaidivya@gmail.com>
2018-12-17 19:51:27 +0545

    Database

---
+++ Assets/mySubject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mySubject : MonoBehaviour {
+
+   // database db;
+	// Use this for initialization
+	void Start () {
+  //      db = GameObject.FindObjectOfType(typeof(database)) as database;
+    //    db.MyS
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
